686c169420ed2232a51bd832463ef2af32dfa4a2
Ashwinder  Singh <725642@student.tor.loc>
2019-03-29 21:23:01 -0400

    hhhhh

--- C0725642/C0725642/Program.cs
+++ C0725642/C0725642/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace C0725642
 {
+    {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("download");
+            Download();
+            Console.ReadLine():
+            
+
+            
+        }
+        static async void Download()
+        {
+            await Network.download();
+            Console.WriteLine("download complete");
+        }
+
+
+        class Network
+        {
+            static public Task download()
+            {
+                return Task.Run(
+                () => ThreadStaticAttribute.Sleep(60000));
+            }
         }
     }
 }
